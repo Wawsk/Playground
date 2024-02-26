83ca79301cc341325304b11680bb93e87031572e
Wawsk <waws1991@gmail.com>
2024-02-26 17:19:35 +0000

    Java input and while loop practice

--- Java/input.java
+++ Java/input.java
@@ -9,5 +9,14 @@ public class input {
         String line = input.nextLine();
 
         System.out.println("You entered: " + line);
+
+        Scanner scanner = new Scanner(System.in);
+        int value = 0;
+        do {
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
+        }
+        while(value != 5);
+        System.out.println("Yes, 5 is the answer");
     }
 }

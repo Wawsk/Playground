1e7760dc9d6ba7a7e87a98b4d6b615827d759470
Wawsk <waws1991@gmail.com>
2024-02-26 11:50:59 +0000

    Java whileLoop

---
+++ Java/whileLoops.java
@@ -0,0 +1,11 @@
+public class whileLoops {
+    public static void main(String[] args) {
+        
+        int value = 0;
+        
+        while(value < 10) {
+            value = value +1;
+            System.out.println(value + ". G'day!");
+        }
+    }
+}

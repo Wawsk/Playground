eb6894b16db1120d966ba578b11d7cde510fcdfd
Wawsk <waws1991@gmail.com>
2024-02-27 16:09:00 +0000

    java arrays

--- Java/arrays.java
+++ Java/arrays.java
@@ -18,5 +18,23 @@ public class arrays {
         for(int i=0; i<numbers.length; i++) {
             System.out.println(numbers[i]);
         }
+        System.out.println();
+
+        String[] words = new String[2];
+
+        words[0] = "Hello ";
+        words[1] = "World!";
+
+        System.out.print(words[0]);
+        System.out.println(words[1]);
+        System.out.println();
+
+        String[] pets = {"cat", "hamster", "dog"};
+
+        for(String pet: pets) {
+            System.out.println(pet);
+        }
+
+        
     }
 }
